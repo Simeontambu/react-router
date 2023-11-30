793d3636e4b220a0119cd4cf28e054367529275f
simeontambu <simeontambu@gmail.com>
2023-11-30 12:48:39 +0100

    create others cmponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login.jsx";
+import SingleProduct from "./pages/singleProduct";
+import About from "./pages/about";
+import Products from "./pages/products";
+import ProductsList from "./pages/productLists";
+import NavBar from "./components/navBar.jsx";
 
 function App() {
-
-
   return (
-   
-     <h1>Hello</h1>
-   
-  )
+    <BrowserRouter>
+   <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/products" element={<Products />}>
+          <Route index element={<ProductsList/>}/>
+          <Route path=":id" element={<SingleProduct/>}/>
+          <Route/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/pages/singleProduct.jsx
+++ src/pages/singleProduct.jsx
@@ -1,5 +1,3 @@
-export default function SingleProduct(){
-    return(
-        <h1>Single product</h1>
-    )
+export default function SingleProduct() {
+  return <h1>Single product</h1>;
 }
